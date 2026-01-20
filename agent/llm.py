2054461e95a05229f683c22dc528f072d68bd94c
Usc-cuhksz <twang121@usc.edu>
2026-01-20 14:55:38 -0800

    Update llm.py configuration

--- agent/llm.py
+++ agent/llm.py
@@ -15,7 +15,7 @@ class LLMClient:
         return completion.choices[0].message.content
 # class LLMClient:
 #     def __init__(self,model_name:str = 'gemini-3-flash-preview'):
-#         self.client = genai.Client(api_key='AIzaSyCKxRgohgFCPmp488rOopxcm2mzDMgKzws')
+#         self.client = genai.Client(api_key='YOUR_API_KEY_HERE')
 #         self.model_name = model_name
 #     def generate(self,prompt:str) -> str:
 #         reponse = self.client.models.generate_content(
